99eb52cfb47e25b39c01b508da84a330dc809742
gkekals <gkekals12@kbu.ac.kr>
2025-08-29 12:45:24 +0900

    1

--- frontend/src/components/home/Aboutme.jsx
+++ frontend/src/components/home/Aboutme.jsx
@@ -1,61 +1,72 @@
 import React from 'react'
 import "./styles/Aboutme.scss"
+import aboutMe from '../../utils/aboutme'
 const Aboutme = () => {
+    const profile = aboutMe.basic
+    const skills = aboutMe.skills
+    const exper = aboutMe.experiences
+    const interests = aboutMe.interests
     return (
-        <section className="aboutme-section">
-            <div className="aboutme-inner">
-                <div className="aboutme-header">
-                    <span className="aboutme-star">*</span>
-                    <span className="aboutme-title">ABOUTME</span>
+        <div className='inner aboutme-inner'>
+            <h1 className="tit">
+                <span className="star-spin"><i className="star">✱</i></span>
+                aboutMe
+            </h1>
+            <div className="t-wrap">
+                <h2 className="sub-tit">
+                    profile
+                </h2>
+                <p className="txt">
+                    들어가는 최종 요소나 사용된다. <br />
+                    레이아웃 들어가는 로렘 부르며,
+                    <br /> 들어가는 최종 전에 프로젝트 사용할 사용된다.
+                </p>
+            </div>
+
+            <div className="about-container">
+                <div className="in-wrap profile-wrap">
+                    <h4 className='list-lst'>profile</h4>
+                    <ul className="lst">
+                        <li>
+                            <strong>이름</strong>: {profile.name}
+                        </li>
+                        <li>
+                            <strong>나이</strong>: {profile.age}
+                        </li>
+                        <li>
+                            <strong>사는곳</strong>: {profile.location}
+                        </li>
+                        <li>
+                            <strong>MBTI</strong>: {profile.mbti}
+                        </li>
+                    </ul>
                 </div>
-                <div className="aboutme-profile">
-                    <h2 className="aboutme-profile-title">profile</h2>
-                    <p className="aboutme-profile-desc">
-                        들어가는 최종 요소나 사용된다. 레이아웃 들어가는 <br />
-                        렘 부르며, 들어가는 최종 전에 프로젝트 사용할 사용된다.<br />
-                        부르며, 채워지기 프로젝트 타이포그래피,
-                    </p>
+                <div className="in-wrap skills-wrap">
+                    <h4 className='list-lst'>Skills</h4>
+                    <ul className="lst">
+                        {skills.map((skill, i) => (
+                            <li key={i}>{skill}</li>
+                        ))}
+                    </ul>
                 </div>
-                <div className="aboutme-menus">
-                    <div className="aboutme-menu">
-                        <h3>menu 1</h3>
-                        <ul>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                        </ul>
-                    </div>
-                    <div className="aboutme-menu">
-                        <h3>menu 1</h3>
-                        <ul>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                        </ul>
-                    </div>
-                    <div className="aboutme-menu">
-                        <h3>menu 1</h3>
-                        <ul>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                        </ul>
-                    </div>
-                    <div className="aboutme-menu">
-                        <h3>menu 1</h3>
-                        <ul>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                            <li>-프로필 관련 내용 넣기/프로필 관련 내용 넣기</li>
-                        </ul>
-                    </div>
+                <div className="in-wrap exper-wrap">
+                    <h4 className='list-lst'>      💼 Experiences</h4>
+                    <ul className="lst">
+                        {exper.map((exp, i) => (
+                            <li key={i}>{exp}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="in-wrap interests-wrap">
+                    <h4 className='list-lst'>           🌱 Interests</h4>
+                    <ul className="lst">
+                        {interests.map((int, i) => (
+                            <li key={i}>{int}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
--- frontend/src/components/home/Work.jsx
+++ frontend/src/components/home/Work.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import "./styles/Work.scss"
+import work from '../../utils/work'
 const Work = () => {
+
     return (
-        <div>Work</div>
+        <ul className="lst">
+            {work.map(item => (
+                <li key={item.id}>
+                    <a className="t-wrap" href={item.links.demo} target="_blank" rel="noopener noreferrer">
+                        <h4 className="list-lst">{item.title}</h4>
+                        <p className="txt">{item.description}</p>
+                        <div className="hash-wrap">
+                            {item.tags.map(tag => (
+                                <span className="hash" key={tag}>{tag}</span>
+                            ))}
+                        </div>
+                    </a>
+                    <a className="img-wrap bg" href={item.links.demo} target="_blank" rel="noopener noreferrer">
+                        <img src={item.thumbnail} alt={item.title} />
+                    </a>
+                </li>
+            ))}
+        </ul>
     )
 }
 
